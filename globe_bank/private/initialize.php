05a897dff76783903f474f67b604db0d7b9398f7
Jose1i1o <guadix88@gmail.com>
2021-11-22 00:55:26 +0100

    output buffering

--- globe_bank/private/initialize.php
+++ globe_bank/private/initialize.php
@@ -1,5 +1,7 @@
 <?php
 
+ob_start(); // Typically this is done in a header file, but we're doing it here for simplicity.  This is a good habit to get into.  It´s mostly done automatically by the framework. You can find the amount of output buffering in the phpinfo() function. Normally it's 4096 bytes. 
+
 define('PRIVATE_PATH', dirname(__FILE__));
 define('PROJECT_PATH', dirname(PRIVATE_PATH));
 define('PUBLIC_PATH', PROJECT_PATH . '/public');
